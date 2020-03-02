45d19e25023951a7bf0eefa0c785a8295970b962
Angel Garcia <angelrodriguez@ucol.mx>
2020-03-01 19:33:52 -0600

    Layout Design

--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -1,5 +1,30 @@
 @extends("layouts.public")
 
 @section("content")
-<h1>Hola</h1>
+<div class="home">
+    <div class="home-gallery">
+        <h1>Esta sera la galeria</h1>
+        <div class="container">
+            <h3>Aqui ira la información</h3>
+        </div>
+    </div>
+    <div class="home-services">
+        <h1>Aqui iran los servicios</h1>
+        <div class="container">
+            <h3>Aqui ira la información</h3>
+        </div>
+    </div>
+    <div class="home-properties">
+        <h1>Aqui iran las propiedades</h1>
+        <div class="container">
+            <h3>Aqui ira la información</h3>
+        </div>
+    </div>
+    <div class="home-comments">
+        <h1>Aqui iran los comentarios</h1>
+        <div class="container">
+            <h3>Aqui ira la información</h3>
+        </div>
+    </div>
+</div>
 @endsection
