e40208cf13985a3311301c479178614add3bfc2d
Angel Garcia <angelrodriguez@ucol.mx>
2020-03-01 19:02:10 -0600

    Layout design

--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -1,24 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends("layouts.public")
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MEXSPACIO Inmobiliarios</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-
-<body>
-    <h1>MEXSPACIO Inmobiliarios</h1>
-    <ul>
-        <li>
-            <a href="{{ url('/') }}">Inicio</a>
-        </li>
-        <li>
-            <a href="{{ url('/nosotros') }}">¿Quienes Somos</a>
-        </li>
-    </ul>
-    <h2>Inicio</h2>
-</body>
-
-</html>
+@section("content")
+<h1>Hola</h1>
+@endsection
